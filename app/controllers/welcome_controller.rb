7a04ae92855b969f1d2ccbf3b696aef951bdb640
Edparra <Edprr4@gmail.com>
2017-04-12 23:05:39 -0400

    logic to pick place

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,26 @@
 class WelcomeController < ApplicationController
   def homepage
-    high = Restaurant.order(:rating).reverse[0]
-    @restaurant = high
-
-  end
+      winners = Restaurant.all
+      highest_yet = -1
+      winners.each do |winner|
+        if winner.visited == nil
+          days = 6
+        else days = Date.today - winner.visited
+          if days > 5
+            days = 5
+            end
+          end
+            penalty = 5 - days
+            new_rating = winner.rating - penalty
+            if new_rating > highest_yet
+              highest_yet = new_rating
+              @restaurant = winner
+            end
+      end
+    end
 end
+
+
 
 
 # high = Restaurant.order(:rating).reverse[0]
