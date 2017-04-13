55969569dd4ead6535a1f264200419f993528b3b
Edparra <Edprr4@gmail.com>
2017-04-13 00:35:58 -0400

    added a seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Restaurant.create!({name:"Burger King", rating:5})
