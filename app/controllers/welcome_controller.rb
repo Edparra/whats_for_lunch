a4ebef1872a031ab601b8c94ac997217e3b07016
Edparra <Edprr4@gmail.com>
2017-04-12 21:34:49 -0400

    master working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,15 @@ class WelcomeController < ApplicationController
   def homepage
     high = Restaurant.order(:rating).reverse[0]
     @restaurant = high
+
   end
 end
 
 
 # high = Restaurant.order(:rating).reverse[0]
-
+#
+#   winners = Restaurant.all
+#   winners.each do |winner|
+#     if winner.visited != null
+#       # is todays date - winner.visited
+#     end
